5fcd3f7bfaa3e1418d0b61292fac3e5d3bb48adc
david <mintsandwitch@gmail.com>
2021-06-20 16:28:05 +0900

    48th problem from leetCode

---
+++ prob48.java
@@ -0,0 +1,44 @@
+public class prob48 {
+    // 솔루션 참조
+    // 접근법 1 : 네 개의 칸을 하나의 그룹으로 묶어 회전
+    public void rotate_group(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+
+    // 접근법 2 : 각 칸의 대각선에 위치한 칸끼리 바꾸고(transpose), 왼쪽과 오른쪽 칸을 바꾼다(reverse left to right).
+    public void rotate_transpose_reverse(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+    }
+
+    public void reflect(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+}
